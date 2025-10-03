7dfbf10b346e2f4452a266f170eb867950899e44
ddingjoo <ddingsha9@teambind.co.kr>
2025-10-03 16:56:05 +0900

    [FEAT] 댓글 서비스 인터페이스 및 주요 기능 구현
    src/main/resources/application-test.yaml: H2 DB 설정 추가 및 테스트 프로파일 생성
    src/main/java/com/teambind/commentserver/service/CommentService.java: 댓글 서비스 인터페이스 정의
    src/main/java/com/teambind/commentserver/service/impl/CommentServiceImpl.java: 댓글 서비스 구현체 작성 (생성, 조회, 수정, 삭제 로직 포함)
    src/main/java/com/teambind/commentserver/repository/CommentRepository.java: 댓글 조회를 위한 커스텀 JPA 메서드 추가
    src/main/java/com/teambind/commentserver/entity/Comment.java: commentId 필드 주석 수정
    build.gradle: 테스트용 H2 DB 의존성 추가
    
    참고: 테스트 코드 작성 및 루트 댓글 조회 최적화 필요
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- src/main/java/com/teambind/commentserver/repository/CommentRepository.java
+++ src/main/java/com/teambind/commentserver/repository/CommentRepository.java
@@ -1,8 +1,15 @@
 package com.teambind.commentserver.repository;
 
 import com.teambind.commentserver.entity.Comment;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CommentRepository extends JpaRepository<Comment, String> {}
+public interface CommentRepository extends JpaRepository<Comment, String> {
+  List<Comment> findByArticleIdAndIsDeletedFalseOrderByCreatedAtAsc(String articleId);
+
+  List<Comment> findByParentCommentIdAndIsDeletedFalseOrderByCreatedAtAsc(String parentCommentId);
+
+  List<Comment> findByRootCommentIdAndIsDeletedFalseOrderByCreatedAtAsc(String rootCommentId);
+}
---
+++ src/main/java/com/teambind/commentserver/service/CommentService.java
@@ -0,0 +1,37 @@
+package com.teambind.commentserver.service;
+
+import com.teambind.commentserver.entity.Comment;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 댓글 서비스 인터페이스
+ *
+ * <p>정상 시나리오 중심의 기능을 정의한다. (예외 케이스 처리는 추후 보강)
+ */
+public interface CommentService {
+
+  /** 루트 댓글 생성 (depth=0, rootCommentId=self) */
+  Comment createRootComment(String articleId, String writerId, String contents);
+
+  /** 부모 댓글에 대한 대댓글 생성 (depth=부모+1) */
+  Comment createReply(String parentCommentId, String writerId, String contents);
+
+  /** 특정 아티클의 삭제되지 않은 전체 댓글 조회 (생성일 오름차순) */
+  List<Comment> getAllCommentsByArticle(String articleId);
+
+  /** 특정 부모 댓글의 자식(대댓글) 조회 */
+  List<Comment> getRepliesByParent(String parentCommentId);
+
+  /** 루트 댓글 기준 스레드 전체 조회 */
+  List<Comment> getThreadByRoot(String rootCommentId);
+
+  /** 단건 조회 */
+  Optional<Comment> getById(String commentId);
+
+  /** 소프트 삭제 */
+  void softDelete(String commentId);
+
+  /** 댓글 내용 수정 */
+  Comment updateContents(String commentId, String newContents);
+}
