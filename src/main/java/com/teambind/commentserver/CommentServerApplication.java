22000c345f5f48754a0402ff2c2e30bd80caed63
ddingjoo <ddingsha9@teambind.co.kr>
2025-10-03 15:57:19 +0900

    [DOC] 프로젝트 Readme 추가 및 초기 코드 정리
    src/main/java/com/teambind/commentserver/CommentServerApplication.java: 코드 들여쓰기 정리 및 불필요한 공백 제거
    Readme.md: 프로젝트 요구사항, ERD, 주요 설계 특징 문서화
    readmeImage/스크린샷 2025-10-03 오후 3.55.53.png: ERD 스크린샷 이미지 추가
    
    참고: Readme는 지속적으로 업데이트 필요
    
    Signed-off-by: ddingjoo <ddingsha9@teambind.co.kr>

--- src/main/java/com/teambind/commentserver/CommentServerApplication.java
+++ src/main/java/com/teambind/commentserver/CommentServerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CommentServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CommentServerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(CommentServerApplication.class, args);
+  }
 }
