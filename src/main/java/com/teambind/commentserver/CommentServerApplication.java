dbffd95bec3b33a271797cccf39792929117ffb9
ddingjoo <ddingsha9@teambind.co.kr>
2025-10-03 12:03:53 +0900

    Initial commit

---
+++ src/main/java/com/teambind/commentserver/CommentServerApplication.java
@@ -0,0 +1,13 @@
+package com.teambind.commentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teambind/commentserver/CommentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teambind.commentserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
